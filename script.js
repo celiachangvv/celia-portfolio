a29b6169d324a6ee2fad3457f321c81ee12cd071
celiachangvv <kaymichang@gmail.com>
2023-12-31 00:39:32 -0800

    homepage update and marketing adding

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-if (window.location.pathname.includes("projects.html")) {
+if (window.location.pathname.includes("projects.html", "social-media-post.html")) {
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {
     scrollFunction();
