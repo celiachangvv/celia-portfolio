bd5c3ba046bac1edbf7af5e6f0369ce69c7b35f0
lidachao1122 <chao.li@usask.ca>
2024-10-25 10:58:47 -0700

    [feat] Add new projects.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+if (window.location.pathname.includes("projects.html")) {
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {
+    scrollFunction();
+  };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
 }
 
